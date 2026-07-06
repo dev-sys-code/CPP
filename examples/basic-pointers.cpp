c6fedcb992a9b2c02336e1e16d0ba2a713eb4757
dev-sys <Kaydenbuster@outlook.com>
2026-07-06 07:41:42 +0100

    Create basic-pointers.cpp

---
+++ examples/basic-pointers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void addBoth(int* numOne, int* numTwo, int* result)
+{
+    *result = *numOne + *numTwo;
+}
+
+int main()
+{
+    using std::cout;
+
+    int numOne = 10;
+    int numTwo = 15;
+    int sum = 0;
+
+    addBoth(&numOne, &numTwo, &sum);
+    cout << sum;
+}
