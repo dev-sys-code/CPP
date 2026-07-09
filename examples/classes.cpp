e2217f6004e703b325082eae1186f57126bb92e3
dev-sys <Kaydenbuster@outlook.com>
2026-07-09 17:35:43 +0100

    Create classes.cpp

---
+++ examples/classes.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+class User {
+private:
+    std::string username{};
+    int age{};
+public:
+    User(std::string username, int age) {
+        this->username = username;
+        this->age = age;
+    }
+    void shout() {
+        std::cout << "Name: " << this->username << " | Age: " << this->age;
+    }
+};
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::getline;
+    using std::string;
+
+    string name{};
+    int age;
+
+    cout << "Name: \n> ";
+    getline(cin, name);
+
+    cout << "Age: \n> ";
+    cin >> age; 
+
+    User u1(name, age);
+    u1.shout();
+    return 0;
+}
