ad5b22653fcca910233257282a96041e0dae685a
dev-sys <Kaydenbuster@outlook.com>
2026-07-11 14:46:03 +0100

    Create password-maker.cpp

---
+++ examples/password-maker.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <random>
+
+int length();
+std::string passwordMaker(const int length);
+
+int main()
+{   
+    int len = length();
+    std::cout << passwordMaker(len);
+}
+
+int length() {
+    int length{};
+
+    std::cout << "Length of password: \n";
+    std::cin >> length;
+
+    return length;
+}
+
+std::string passwordMaker(const int length) {
+    std::string chars{"ABCDEFGHIJKLMNOPQRSTUVWXZYabcdefhijklmnopqrstuvxyz123456789!?$"};
+    std::string password{};
+
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> ran(0, chars.size() - 1);
+
+    for (int i{}; i < length; i++) {
+        password += chars[ran(gen)];
+    }
+
+    return password;
+}
