c40a506cd9475118cff64c34dc36fae027287be5
dev-sys-code <kaydenbuster@outlook.com>
2026-07-12 11:04:03 +0100

    read/write

--- fstream-filesystem/second-file-system.cpp
+++ fstream-filesystem/second-file-system.cpp
@@ -4,12 +4,14 @@
 
 void textForFile(std::string& text);
 void fileWrite(const std::string& text);
+void fileRead();
 
 int main() {
     std::string text{};
 
     textForFile(text);
     fileWrite(text);
+    fileRead();
     return 0;
 }
 
@@ -26,5 +28,16 @@ void fileWrite(const std::string& text) {
         std::cout << "Successfuly writen to file";
     } else {
         std::cerr << "Error";
+    }
+}
+
+void fileRead() {
+    std::ifstream inFile("text.txt");
+    std::string line{};
+
+    if (inFile.is_open()) {
+        while (std::getline(inFile, line)) {
+            std::cout << line << "\n";
+        }
     }
 }
