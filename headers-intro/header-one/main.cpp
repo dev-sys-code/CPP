97bb3ec1391d36388b930152cb36a3d78449e37e
dev-sys <Kaydenbuster@outlook.com>
2026-07-13 16:26:38 +0100

    Update main.cpp

--- headers-intro/header-one/main.cpp
+++ headers-intro/header-one/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-#include "add.h"
+#include "functions.h"
 
 int main() {
-    int result{add(5, 7)};
-    std::cout << "Sum: " << result << "\n";
+    int number{10}, numberTwo{5}, result{};
+
+    result = add(number, numberTwo);
+    std::cout << number << " + " << numberTwo << " = " << result << "\n";
+
+    result = sub(number, numberTwo);
+    std::cout << number << " - " << numberTwo << " = " << result << "\n";
+
+    result = mul(number, numberTwo);
+    std::cout << number << " * " << numberTwo << " = " << result << "\n";
     return 0;
 }
