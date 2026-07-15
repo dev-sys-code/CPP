0765215abaa6746547b355685b5775882ca71153
Kayden <Kaydenbuster@outlook.com>
2026-07-15 07:43:32 +0100

    Update main.cpp

--- examples/bank/main.cpp
+++ examples/bank/main.cpp
@@ -10,20 +10,24 @@ int main()
     using std::system;
 
     int choice{};
-    float balance{100}, amount{};
+    float balance{100}, amount{}, cash{100};
 
     while (true)
         {
         try
         {   
-            cout << "\nBalance: " << balance << "\n\n(1) Withdraw\n(2) Deposit\n\n(0) Exit\n    >_ ";
+            cout
+                << "\nBalance: " << balance << "\nCash: " << cash
+                << "\n\n(1) Withdraw\n(2) Deposit\n\n(0) Exit\n    >_ ";
+
             cin.clear();
             cin >> choice;
 
             switch (choice)
             {   
                 case 0: return 0;
-                case 1: withdraw(balance, amount); break;
+                case 1: withdraw(balance, amount, cash); break;
+                case 2: deposit(balance, amount, cash); break;
                 default: throw std::out_of_range("Keep between (0-3)");
             }
         }
