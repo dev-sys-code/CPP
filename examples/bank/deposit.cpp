963bec6cf20e32a16c1f529c1590fd07d246e9e9
Kayden <Kaydenbuster@outlook.com>
2026-07-15 07:43:09 +0100

    Update deposit.cpp

--- examples/bank/deposit.cpp
+++ examples/bank/deposit.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include "deposit.h"
 
+void deposit(float& balance, float& amount, float& cash)
+{
+    using std::cout;
+    using std::cin;
+
+
+
+    try
+    {
+    cout << "\nEnter amount to deposit:\n    > ";
+    cin >> amount;
+        if (amount > cash)
+        {   
+            cout << "\nCash: " << cash << "\n";
+            throw std::out_of_range("Invalid amount");
+        }
+        balance += amount;
+        cash -= amount;
+    }
+    catch (const std::invalid_argument& e)
+    {
+        std::cerr << "Error: " << e.what();
+    }
+    catch (const std::out_of_range& e)
+    {
+        std::cerr << "Error: " << e.what();
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown error occured";
+    }
+}
