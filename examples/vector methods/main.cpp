7ba88bc0e9ca820590af4df1d9a60a503dbc9d1e
Kayden <Kaydenbuster@outlook.com>
2026-07-21 12:17:16 +0100

    Update main.cpp

--- examples/vector methods/main.cpp
+++ examples/vector methods/main.cpp
@@ -7,6 +7,7 @@ int main() {
     numbers.push_back(5);                   // push_back(val) -> Adds 5 to the end
     numbers.pop_back();                     // pop_back() -> Deletes last element, 5 in this case
     numbers.insert(numbers.begin() + 4, 5); // insert(pos, val) -> Drops 5 at index 4
+    numbers.erase(numbers.begin() + 5);     // erase(pos, val) -> Erase the 5th element
 
     for (int i : numbers) {
         std::cout << i << " ";
