11fcc192f3ce0cb5855c5ae95e1f33d4c9f685b9
Kayden <Kaydenbuster@outlook.com>
2026-07-21 18:11:10 +0100

    Update main.cpp

--- mini-projects/main.cpp
+++ mini-projects/main.cpp
@@ -1,1 +1,35 @@
+// BROKEN
 
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    using std::cout;
+    using std::cin;
+    using std::vector;
+
+    vector<double> numbers{};
+    double temp{};
+
+    for (int i{}; i < 5; i++) {
+        cout << "Number:\n>_";
+        cin >> temp;
+        numbers.push_back(temp);
+    }
+
+    temp = 0;
+    std::string alltogther{};
+
+    for (int i : numbers) {
+        temp += i;
+
+        alltogther += std::to_string(numbers[i]);
+
+        if (i < numbers.size() - 1) {
+            alltogther += " + ";
+        }
+    }
+
+    cout << alltogther << " = " << temp;
+}
