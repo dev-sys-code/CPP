b19e6baba57a4ba9b587097e18cc20256fbb6d3a
Kayden <Kaydenbuster@outlook.com>
2026-07-21 12:14:51 +0100

    Create main.cpp

---
+++ examples/vector methods/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> numbers{2, 3, 1, 4};
+
+    numbers.push_back(5);                   // push_back(val) -> Adds 5 to the end
+    numbers.pop_back();                     // pop_back() -> Deletes last element, 5 in this case
+    numbers.insert(numbers.begin() + 4, 5); // insert(pos, val) -> Drops 5 at index 4
+
+    for (int i : numbers) {
+        std::cout << i << " ";
+    }
+}
