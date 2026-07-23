f53606d3a0230e13731f48f1e442242ea0b63c73
Kayden <Kaydenbuster@outlook.com>
2026-07-23 21:37:15 +0100

    Create main.cpp

---
+++ raylib/basics/save-two/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "raylib.h"
+#include <random>
+
+struct Entity {
+    Vector2 position;
+    Vector2 speed;
+    float radius;
+    Color color;
+};
+
+void reset(Entity &player, const int WIDTH, const int HEIGHT);
+int randomNumberGen(const int SIZE);
+
+int main() {
+    const int WIDTH{800};
+    const int HEIGHT{450};
+    InitWindow(WIDTH, HEIGHT, "Raylib");
+
+    Texture2D background{LoadTexture("images/Background.png")};
+
+    Entity player {
+        { (float)WIDTH / 2, (float)HEIGHT / 2 },
+        { 4.0f, 4.0f},
+        20.0f,
+        BLUE
+    };
+
+    Entity enemy {
+        { (float)randomNumberGen(WIDTH) - 20.0f, (float)randomNumberGen(HEIGHT) - 20.0f},
+        { 4.0f, 4.0f },
+        20.0f,
+        RED
+    };
+    
+    SetTargetFPS(144);
+
+    while (!WindowShouldClose()) {
+        if (IsKeyDown(KEY_W)) player.position.y -= player.speed.y;
+        if (IsKeyDown(KEY_S)) player.position.y += player.speed.y;
+        if (IsKeyDown(KEY_A)) player.position.x -= player.speed.x;
+        if (IsKeyDown(KEY_D)) player.position.x += player.speed.x;
+
+        if (IsKeyDown(KEY_R)) reset(player, WIDTH, HEIGHT);
+
+        BeginDrawing();
+
+        DrawTexture(background, 0, 0, WHITE);
+        DrawCircleV(player.position, player.radius, player.color);
+        DrawCircleV(enemy.position, enemy.radius, enemy.color);
+        
+        EndDrawing();
+    }
+
+    UnloadTexture(background);
+    CloseWindow();
+    return 0;
+}
+
+void reset(Entity &player, const int WIDTH, const int HEIGHT) {
+    player.position.x = (float)WIDTH / 2;
+    player.position.y = (float)HEIGHT / 2;
+}
+
+int randomNumberGen(const int SIZE) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> random(0, SIZE);
+
+    return random(gen);
+}
