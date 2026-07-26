91883b3f56f62ffa671257d9223ebe7c0dc2f01c
Kayden <Kaydenbuster@outlook.com>
2026-07-26 22:00:55 +0100

    main

---
+++ interpreter/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+int main() {
+    std::string code{"a_var = 90"};
+    size_t equalPos{code.find('=')};
+
+    std::string varName{code.substr(0, equalPos)};
+
+    while (!varName.empty() && varName.back() == ' ') {
+        varName.pop_back();
+    }
+
+    std::string valStr{code.substr(equalPos + 1)};
+
+    int val{std::stoi(valStr)};
+
+    std::unordered_map<std::string, int> memory;
+    memory[varName] = val;
+
+    std::cout << "Variable: '" << varName << "' is stored with value: " << val;
+
+    return 0;
+}
