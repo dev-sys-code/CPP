7e2c3f59622c9afb39d7e17c891762b68eab4bf1
Kayden <Kaydenbuster@outlook.com>
2026-07-28 09:48:43 +0100

    Update main.cpp

--- raylib/snake/main.cpp
+++ raylib/snake/main.cpp
@@ -17,6 +17,8 @@ int main() {
     const int WIDTH{600};
     const int HEIGHT{600};
     float currentPos{40.0f};
+    int direction{};
+
     InitWindow(WIDTH, HEIGHT, "Game");
 
     
@@ -35,19 +37,19 @@ int main() {
 
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) {
-
+            direction = 1;
         }
 
         if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_RIGHT)) {
-
+            direction = 2;
         }
 
         if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) {
-
+            direction = 3;
         }
 
         if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_LEFT)) {
-
+            direction = 4;
         }
 
         BeginDrawing(); 
