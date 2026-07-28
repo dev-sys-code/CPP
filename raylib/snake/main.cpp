ed6063c0fd8ca12bacce952735e4899835b5fb69
Kayden <Kaydenbuster@outlook.com>
2026-07-28 09:35:50 +0100

    Update main.cpp

--- raylib/snake/main.cpp
+++ raylib/snake/main.cpp
@@ -2,18 +2,19 @@
 #include <vector>
 #include "raylib.h"
 
+struct Player {
+    Vector2 position;
+    Vector2 velocity;
+    int boxes; // size of snake
+    const float size{35.0f}; // size of each 'box'
+};
+
 int main() {
     const int WIDTH{600};
     const int HEIGHT{600};
     float currentPos{40.0f};
     InitWindow(WIDTH, HEIGHT, "Game");
 
-    struct Player {
-        Vector2 position;
-        Vector2 velocity;
-        int boxes; // size of snake
-        const float size{35.0f}; // size of each 'box'
-    };
     
     Player player {
         { WIDTH / 2, HEIGHT / 2 },
@@ -26,16 +27,18 @@ int main() {
         { WIDTH / 2, HEIGHT / 2 + 40.0f }
     };
 
+    body.push_back({WIDTH / 2, HEIGHT / 2 + 80.0f});
+
     while (!WindowShouldClose()) {
         BeginDrawing(); 
-        ClearBackground(RAYWHITE);
-            for (int i{body.size() - 1}; i >= 0; i--) {
-                if (i == 0) {
-                    DrawRectangleV(body[i], { player.size, player.size }, RED);
-                } else {
-                    DrawRectangleV(body[i], { player.size, player.size }, BLACK);
+            ClearBackground(RAYWHITE);
+                for (int i{body.size() - 1}; i >= 0; i--) {
+                    if (i == 0) {
+                        DrawRectangleV(body[i], { player.size, player.size }, RED);
+                    } else {
+                        DrawRectangleV(body[i], { player.size, player.size }, BLACK);
+                    }
                 }
-            }
         EndDrawing();
     }
 
