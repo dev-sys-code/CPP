2c6018962f7d1d93363c32b0995d491d6e49efb1
Kayden <Kaydenbuster@outlook.com>
2026-07-31 09:05:19 +0100

    Update main.cpp

--- raylib/car-game/main.cpp
+++ raylib/car-game/main.cpp
@@ -14,13 +14,24 @@ int main() {
     Player player {
         { ((float)HEIGHT / 2.0f) - 17.5f, ((float)WIDTH / 2.0f) - 17.5f },
         { 400.0f, 400.0f },
-        { 35.0f, 35.0f }
+        { 50.0f, 85.0f }
     };
 
     InitWindow(WIDTH, HEIGHT, "Car game");
 
     while (!WindowShouldClose()) {
+        if (IsKeyDown(KEY_W)) player.position.y -= player.velocity.y * GetFrameTime();
+        if (IsKeyDown(KEY_A)) player.position.x -= player.velocity.x * GetFrameTime();
+        if (IsKeyDown(KEY_S)) player.position.y += player.velocity.y * GetFrameTime();
+        if (IsKeyDown(KEY_D)) player.position.x += player.velocity.x * GetFrameTime();
+
+        if (player.position.y < 0) player.position.y = 0;
+        if (player.position.y > HEIGHT - player.size.y) player.position.y = HEIGHT - player.size.y;
+        if (player.position.x < 0) player.position.x = 0;
+        if (player.position.x > WIDTH - player.size.x) player.position.x = WIDTH - player.size.x;
+        
         BeginDrawing();
+            ClearBackground(RAYWHITE);
             DrawRectangleV(player.position, player.size, BLUE);
         EndDrawing();
     }
