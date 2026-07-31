743e1fe03ed60c9ca2e61dd799efa939739cf758
Kayden <Kaydenbuster@outlook.com>
2026-07-31 08:55:46 +0100

    add player

--- raylib/car-game/main.cpp
+++ raylib/car-game/main.cpp
@@ -11,10 +11,17 @@ int main() {
     int WIDTH{600};
     int HEIGHT{600};
 
+    Player player {
+        { ((float)HEIGHT / 2.0f) - 17.5f, ((float)WIDTH / 2.0f) - 17.5f },
+        { 400.0f, 400.0f },
+        { 35.0f, 35.0f }
+    };
+
     InitWindow(WIDTH, HEIGHT, "Car game");
 
     while (!WindowShouldClose()) {
         BeginDrawing();
+            DrawRectangleV(player.position, player.size, BLUE);
         EndDrawing();
     }
 
