a33861282d82a9469bc860b9061f7dd35c1e924b
Kayden <Kaydenbuster@outlook.com>
2026-07-31 09:08:51 +0100

    Update main.cpp

--- raylib/car-game/main.cpp
+++ raylib/car-game/main.cpp
@@ -1,7 +1,16 @@
 #include "raylib.h"
 #include <iostream>
+#include <random>
+
+int random(const int num);
 
 struct Player {
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 size;
+};
+
+struct Enemy {
     Vector2 position;
     Vector2 velocity;
     Vector2 size;
@@ -13,6 +22,12 @@ int main() {
 
     Player player {
         { ((float)HEIGHT / 2.0f) - 17.5f, ((float)WIDTH / 2.0f) - 17.5f },
+        { 400.0f, 400.0f },
+        { 50.0f, 85.0f }
+    };
+
+    Enemy enemy {
+        {  },
         { 400.0f, 400.0f },
         { 50.0f, 85.0f }
     };
@@ -38,4 +53,11 @@ int main() {
 
     CloseWindow();
     return 0;
+}
+
+int random(const int num) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> random(10, num);
+    return random(gen);
 }
