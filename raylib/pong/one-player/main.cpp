c5fed9318021e79cb858d02db28e17abb492534a
Kayden <Kaydenbuster@outlook.com>
2026-08-01 17:02:39 +0100

    Update main.cpp

--- raylib/pong/one-player/main.cpp
+++ raylib/pong/one-player/main.cpp
@@ -117,6 +117,6 @@ int randomNum(const int HEIGHT) {
     static std::random_device rd;
     static std::mt19937 gen(rd());
 
-    std::uniform_int_distribution random(50, HEIGHT - 50);
+    std::uniform_int_distribution<> random(50, HEIGHT - 50);
     return random(gen);
 }
