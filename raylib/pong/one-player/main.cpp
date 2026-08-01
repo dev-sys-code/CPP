c7212e5cf5065c545d8748b0726a25aa5fba7988
Kayden <Kaydenbuster@outlook.com>
2026-08-01 17:37:45 +0100

    Update main.cpp

--- raylib/pong/one-player/main.cpp
+++ raylib/pong/one-player/main.cpp
@@ -1,12 +1,13 @@
+/*
+(LOCAL): Two player pong game (W + S (player one)), (I + K(player two))
+*/
+
 #include "raylib.h"
-#include <iostream>
-#include <random>
 
-int randomNum(const int HEIGHT);
-
-struct Player {
+struct Entity {
     Vector2 position;
-    float speed;
+    Vector2 velocity;
+    Vector2 size;
 };
 
 struct Ball {
@@ -15,108 +16,72 @@ struct Ball {
     float radius;
 };
 
-struct CPU {
-    Vector2 position;
-    float speed;
-};
-
 int main() {
-    const int WIDTH{850};
-    const int HEIGHT{400};
-    int score{}, cpu_score{};
+    const int WIDTH{800}, HEIGHT{450};
 
-    InitWindow(WIDTH, HEIGHT, "RAYLIB");
-    SetTargetFPS(144);
+    int p1Score{}, p2Score{};
 
-    Player player {
-        { 20.0f, (HEIGHT / 2) - 100 / 2},
-        { 400.0f }
+    InitWindow(WIDTH, HEIGHT, "Pong Game");
+
+    Entity playerOne {
+        { 50, HEIGHT / 2 - 50 },
+        { 500.0f, 500.0f },
+        { 20.0f, 100.0f }
+    };
+
+    Entity playerTwo {
+        { WIDTH - 50, HEIGHT / 2 - 50 },
+        { 500.0f, 500.0f },
+        { 20.0f, 100.0f }
     };
 
     Ball ball {
-        { WIDTH / 2, randomNum(HEIGHT) },
-        { 500.0f, 500.0f },
+        { WIDTH / 2, HEIGHT / 2 },
+        { 400.0f, 400.0f },
         { 15.0f }
     };
 
-    CPU cpu {
-        { WIDTH - 50.0f, (HEIGHT / 2) - 100 / 2 },
-        { 350.0f }
-    };
-
-    Texture2D background{LoadTexture("images/Background.png")};
-
     while (!WindowShouldClose()) {
-        float dt{GetFrameTime()};
+        if (IsKeyDown(KEY_W)) playerOne.position.y -= playerOne.velocity.y * GetFrameTime();
+        if (IsKeyDown(KEY_S)) playerOne.position.y += playerOne.velocity.y * GetFrameTime();
+        if (IsKeyDown(KEY_I)) playerTwo.position.y -= playerTwo.velocity.y * GetFrameTime();
+        if (IsKeyDown(KEY_K)) playerTwo.position.y += playerTwo.velocity.y * GetFrameTime();
 
-        if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) player.position.y -= player.speed * dt;
-        if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) player.position.y += player.speed * dt;
+        if (playerOne.position.y < 0) playerOne.position.y = 0;
+        if (playerOne.position.y > HEIGHT - playerOne.size.y) playerOne.position.y = HEIGHT - playerOne.size.y;
+        if (playerTwo.position.y < 0) playerTwo.position.y = 0;
+        if (playerTwo.position.y > HEIGHT - playerTwo.size.y) playerTwo.position.y = HEIGHT - playerTwo.size.y;
 
+        if (ball.position.y <= 0) ball.velocity.y *= -1.0f;
+        if (ball.position.y > HEIGHT) ball.velocity.y  *= -1.0f;
+        if (ball.position.x <= 0) ball.velocity.x *= -1.0f;
+        if (ball.position.x > WIDTH) ball.velocity.x *= -1.0f;
 
-        if (cpu.position.y + 50.0f < ball.position.y) {
-            cpu.position.y += cpu.speed * dt;
-        } else if (cpu.position.y + 50.0f > ball.position.y) {
-            cpu.position.y -= cpu.speed * dt;
-        }
+        ball.position.y -= ball.velocity.y * GetFrameTime();
+        ball.position.x -= ball.velocity.x * GetFrameTime();
 
-        if (cpu.position.y < 0) cpu.position.y = 0;
-        if (cpu.position.y > HEIGHT - 100) cpu.position.y = HEIGHT - 100;
+        Rectangle playerOneRect = {playerOne.position.x, playerOne.position.y, playerOne.size.x, playerOne.size.y};
+        Rectangle playerTwoRect = {playerTwo.position.x, playerTwo.position.y, playerTwo.size.x, playerTwo.size.y};
 
-        if (player.position.y < 0) player.position.y = 0;
-        if (player.position.y > HEIGHT - 100) player.position.y = HEIGHT - 100;
-
-        ball.position.x += ball.velocity.x * dt;
-        ball.position.y += ball.velocity.y * dt;
-
-        if (ball.position.y - ball.radius <= 0 || ball.position.y + ball.radius >= HEIGHT) {
-            ball.velocity.y *= -1.0f;
-        }
-
-        if (ball.position.x + ball.radius >= WIDTH) {
+        if (CheckCollisionCircleRec(ball.position, ball.radius, playerOneRect) && ball.velocity.x > 0) {
             ball.velocity.x *= -1.0f;
-            score ++;
+            p1Score ++;
         }
 
-        if (ball.position.x - ball.radius <= 0) {
+        if (CheckCollisionCircleRec(ball.position, ball.radius, playerTwoRect) && ball.velocity.x < 0) {
             ball.velocity.x *= -1.0f;
-            cpu_score ++;
+            p2Score ++;
         }
-
-        Rectangle playerRect{ player.position.x, player.position.y, 30.0f, 100.0f };
-        Rectangle cpuRect{ cpu.position.x, cpu.position.y, 30.0f, 100.0f };
-
-        if (CheckCollisionCircleRec(ball.position, ball.radius, playerRect)  && ball.velocity.x < 0) {
-            ball.velocity.x *= -1.0f;
-            score ++;
-        }
-
-        if (CheckCollisionCircleRec(ball.position, ball.radius, cpuRect)  && ball.velocity.x > 0) {
-            ball.velocity.x *= -1.0f;
-            cpu_score++;
-        }
+        
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawTexture(background, 0, 0, WHITE);
-            DrawText(TextFormat("%i", score), WIDTH / 4, HEIGHT / 2 - 50, 100.0f, BLACK);
-            DrawText("|", WIDTH / 2, HEIGHT / 2 - 50, 100.0f, BLACK);
-            DrawText(TextFormat("%i", cpu_score), WIDTH - WIDTH / 4, HEIGHT / 2 - 50, 100.0f, BLACK);
-    
-            DrawRectangle(player.position.x, player.position.y, 30, 100, BLACK); // player paddle
-            DrawCircleV(ball.position, ball.radius, BLACK); // ball
-            DrawRectangle(cpu.position.x, cpu.position.y, 30, 100, BLACK); // cpu paddle
+            DrawRectangleV(playerOne.position, playerOne.size, BLACK);
+            DrawRectangleV(playerTwo.position, playerTwo.size, BLACK);
+            DrawCircleV(ball.position, ball.radius, BLACK);
+            DrawText(TextFormat("Score: %i", p1Score), 100, 100, 20, BLACK);
+            DrawText(TextFormat("Score: %i", p2Score), WIDTH - 100, 100, 20,BLACK);
         EndDrawing();
     }
-
-    UnloadTexture(background);
-    CloseWindow();
     return 0;
-}
-
-int randomNum(const int HEIGHT) {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-
-    std::uniform_int_distribution<> random(50, HEIGHT - 50);
-    return random(gen);
 }
