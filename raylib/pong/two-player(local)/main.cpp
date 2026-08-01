98e6b7153a23ed65f33e7696652b5c017b3af095
Kayden <Kaydenbuster@outlook.com>
2026-08-01 17:10:27 +0100

    Update main.cpp

--- raylib/pong/two-player(local)/main.cpp
+++ raylib/pong/two-player(local)/main.cpp
@@ -52,13 +52,14 @@ int main() {
         if (playerTwo.position.y < 0) playerTwo.position.y = 0;
         if (playerTwo.position.y > HEIGHT - playerTwo.size.y) playerTwo.position.y = HEIGHT - playerTwo.size.y;
 
-        if (ball.position.y < 0) ball.position.y *= -1.0f;
-        if (ball.position.y > HEIGHT - ball.radius) ball.position.y *= -1.0f;
-        if (ball.position.x < 0) ball.position.x *= -1.0f;
-        if (ball.position.x > WIDTH - ball.radius) ball.position.x *= -1.0f;
+        if (ball.position.y < 0) ball.velocity.y *= -1.0f;
+        if (ball.position.y > HEIGHT) ball.velocity.y  *= -1.0f;
+        if (ball.position.x < 0) ball.velocity.x *= -1.0f;
+        if (ball.position.x > WIDTH) ball.velocity.x *= -1.0f;
 
         ball.position.y -= ball.velocity.y * GetFrameTime();
         ball.position.x -= ball.velocity.x * GetFrameTime();
+        
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawRectangleV(playerOne.position, playerOne.size, BLACK);
