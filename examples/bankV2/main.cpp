dec71b8c640a617817b7179a4716217dfbe7dc28
Kayden <Kaydenbuster@outlook.com>
2026-08-03 09:08:32 +0100

    Update main.cpp

--- examples/bankV2/main.cpp
+++ examples/bankV2/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
 int main() {
+    int choice{};
+
     while (true) {
         std::cout 
         << "(1) Withdraw\n"
         << "(2) Deposit\n"
-        << "(3) View Balance\n";
+        << "(3) View Balance\n\n"
+        << "(0) Exit\n>_";
+        std::cin >> choice;
+        switch (choice) {
+            case 0:
+            case 1: 
+            case 2:
+            case 3: 
+            default:
+        }
     }
 }
