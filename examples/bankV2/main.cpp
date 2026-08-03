0e2759d94b4b02182a7e47a2764f06eca3d4f1c4
Kayden <Kaydenbuster@outlook.com>
2026-08-03 09:18:07 +0100

    Update main.cpp

--- examples/bankV2/main.cpp
+++ examples/bankV2/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+void error(const std::string msg) {
+    std::cout << "[Error] " << msg << "\n";
+}
+
 int main() {
     int choice{};
     double money{}, card{};
@@ -12,13 +16,13 @@ int main() {
         << "(0) Exit\n>_";
 
         std::cin >> choice;
-        
+
         switch (choice) {
             case 0:
             case 1: 
             case 2:
             case 3: 
-            default:
+            default: error("Not in range");
         }
     }
 }
