eeb26c7b9e9ea4b079c8d46ec1e1793609b399b5
Kayden <Kaydenbuster@outlook.com>
2026-08-03 11:10:46 +0100

    Update withdraw.cpp

--- examples/bankV2/hdrsrc/withdraw.cpp
+++ examples/bankV2/hdrsrc/withdraw.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include "./headers/withdraw.hpp"
+
+void buy(double& money, double& card, const double amount) {
+    card -= amount;
+    std::cout << "Thanks for using us!\n";
+}
+
+void withdraw(double& money, double& card, const double amount) {
+    int choice{};
+
+    if (amount > card) {
+        std::cout << "Insufficient balance: £" << card << " available\n";
+    } else if (amount > card / 2) {
+        while (true) {
+            std::cout << "£" << amount << " is over 50% of your balance, are you sure? (1 - yes / 0 - no)";
+            std::cin >> choice;
+            switch (choice) {
+                case 0: break;
+                case 1:
+                    buy(money, card, amount);
+                    break; 
+            }
+            buy(money, card, amount);
+        }
+    }
+}
 
