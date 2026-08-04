9f6d340d7d92b16a035c76b7729b0d1c0587caba
Kayden <Kaydenbuster@outlook.com>
2026-08-04 08:45:12 +0100

    Update main.cpp

--- examples/bankV2/main.cpp
+++ examples/bankV2/main.cpp
@@ -2,14 +2,14 @@
 #include "withdraw.hpp"
 #include "deposit.hpp"
 
-class Person {
+class Bank {
 private:
     double money{50};
     double card{50};
     double amount{};
 
 public:
-    Person(double m, double c, double a) {
+    Bank(double m, double c, double a) {
         this->money = m;
         this->card = c;
         this->amount = a;
@@ -38,7 +38,7 @@ void error(const std::string msg) { std::cout << "[Error] " << msg << "\n"; }
 
 int main() {
     int choice{}, temp{};
-    double money{50}, card{50}, amount{};
+    Bank bankOne(50, 50, 0);
 
     while (true) {
         std::cout 
@@ -50,27 +50,6 @@ int main() {
 
         std::cin >> choice;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-        switch (choice) {
-            case 0: return 0;
-            case 1:
-                std::cout << "£";
-                std::cin >> amount;
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                withdraw(money, card, amount); break;
-
-            case 2:
-                std::cout << "£";
-                std::cin >> amount;
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                deposit(money, card, amount); break;
-
-            case 3: 
-                std::cout << "\n\nCash: £" << money << " | Card: £" << card << "\n"; 
-                continue;
-
-            default: error("Not in range"); break;
-        }
     }
     return 0;
 }
