66d1934cb576edc317cbc99f9884ee72b021ac41
Kayden <Kaydenbuster@outlook.com>
2026-08-04 08:16:20 +0100

    Update deposit.cpp

--- examples/bankV2/hdrsrc/deposit.cpp
+++ examples/bankV2/hdrsrc/deposit.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include "deposit.hpp"
 
+void buy(double& money, double& card, const double amount) {
+    money -= amount;
+    card += amount;
+    std::cout << "Thanks for using us!\n";
+}
+
+void deposit(double& money, double& card, const double amount) {
+    int choice{};
+
+    if (amount > money) {
+        std::cout << "Insufficient balance: £" << money << " available\n";
+    } else if (amount > money / 2) {
+        while (true) {
+            std::cout << "£" << amount << " is over 50% of your balance, are you sure? (1 - yes / 0 - no\n";
+            std::cin >> choice;
+            if (choice == 0) {
+                std::cout << "\nYou have not been charged";
+            } else {
+                buy(money, card, amount);
+            }
+            break;
+        }
+    }
+    else {
+        buy(money, card, amount);
+    }
+}
