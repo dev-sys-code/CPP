2113377bb3a1608530c1974c3e4eedafbf298d7d
Kayden <Kaydenbuster@outlook.com>
2026-08-04 18:59:38 +0100

    Update renameBank.cpp

--- examples/bankV2/hdrsrc/renameBank.cpp
+++ examples/bankV2/hdrsrc/renameBank.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include "renameBank.hpp"
 
+void renameBank(std::vector<std::string>& banks) {
+    {
+        int j{};
+        for (auto i : banks) {
+            std::cout << "()" << j << ") " << i ;
+        }
+    }
+}
