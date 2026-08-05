6a9ce99805ee2dbdc28d29619399f08a03bfa71a
Kayden <Kaydenbuster@outlook.com>
2026-08-05 07:45:33 +0100

    Update renameBank.cpp

--- examples/bankV2/hdrsrc/renameBank.cpp
+++ examples/bankV2/hdrsrc/renameBank.cpp
@@ -2,10 +2,13 @@
 #include "renameBank.hpp"
 
 void renameBank(std::vector<std::string>& banks) {
+    int choice{};
     {
         int j{};
         for (auto i : banks) {
-            std::cout << "()" << j << ") " << i ;
+            std::cout << "(" << j << ") " << i ;
         }
     }
+    std::cout << "Which Bank are you renaming?\n>_ ";
+    std::cin >> choice;
 }
